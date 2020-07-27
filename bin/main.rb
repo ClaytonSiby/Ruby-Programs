a794afc8e4d67310e127403360b9f170998ac601
ClaytonSiby <claytonsiby@gmail.com>
2020-07-27 11:37:53 +0200

    add game logic in main file

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,88 @@
 # frozen_string_literal: true
 
+require_relative '../lib/board'
+require_relative '../lib/player.rb'
+
+# PlayGame module holding the game possible wins.
+module PlayGame
+  POSSIBLE_WINS = [
+    [1, 2, 3], [4, 5, 6],
+    [7, 8, 9], [1, 4, 7],
+    [2, 5, 8], [3, 6, 9],
+    [3, 5, 7], [1, 5, 9]
+  ].freeze
+
+  # check if array includes give element.
+  class CheckArr
+    def does_contain?(arr)
+      arr.all? { |el| include?(el) }
+    end
+  end
+
+  def self.won?(player)
+    POSSIBLE_WINS.any? { |element| player.choices.does_contain?(element) }
+  end
+end
+
+def name_players
+  name = gets.chomp
+  name = gets.chomp while name.empty
+
+  name
+end
+
+def go_player(player)
+  puts "#{player.name} select (in range 1..9) a number to make a move >> "
+  choice = gets.chomp.strip.to_i
+  choice_result = player.make_move(choice)
+  while choice_result != choice
+    puts choice_result
+    choice = gets.chomp.strip.to_i
+  end
+end
+
+def announce_winner?(player)
+  if PlayGame.won?(player)
+    puts "Congatulations #{player.name} you've won the game!!"
+    return true
+  end
+  false
+end
+
+puts 'First player please state your name >> '
+first_player = Player.new(name_players, 'X')
+
+puts 'Second player please enter your name >> '
+first_player = Player.new(name_players, 'X')
+
+puts "Game's gonna be between #{fist_player.name} (X) & #{second_player} (O). Good luck!!!"
+
+loop do
+  replay = false
+  game_on = 0
+  sleep(2)
+  while game_on < 9
+    puts Boad.update_board
+    game_on(first_player)
+    puts Board.update_board
+    break if announce_winner?(first_player)
+
+    game_on += 1
+    break if game_on > 8
+
+    puts Boad.update_board
+    game_on(first_player)
+    puts Board.update_board
+    break if announce_winner?(second_player)
+
+    game_on += 1
+  end
+  puts 'Game over & it is a draw' if game_on > 8
+  puts 'Do you wish to replay? (Y/N) >> '
+  answer = gets.chomp.upcase
+  replay = true if answer == 'Y'
+  Board.reset_board
+  first_player.reset_choices
+  second_player.reset_choices
+  break unless replay
+end
--- lib/board.rb
+++ lib/board.rb
@@ -1,31 +1,39 @@
 # frozen_string_literal: true
 
+require_relative '../lib/player'
+
 # board class definition
 class Board
+  @@positions = (1..9).to_a
   def initialize
-    @positions = (1..9).to_a
+    @@positions
   end
 
   def self.draw_board
     "
-    #{@positions[0]} | #{@positions[1]} | #{@positions[2]}
+    #{@@positions[0]} | #{@@positions[1]} | #{@@positions[2]}
     =======
-    #{@positions[3]} | #{@positions[4]} | #{@positions[5]}
+    #{@@positions[3]} | #{@@positions[4]} | #{@@positions[5]}
     =======
-    #{@positions[6]} | #{@positions[7]} | #{@positions[8]}
+    #{@@positions[6]} | #{@@positions[7]} | #{@@positions[8]}
     "
   end
 
+  def self.update_board
+    system('clear')
+    Board.draw_board
+  end
+
   def self.valid_move?(move)
-    @positions.include?(move)
+    move >= 1 && move <= 9 ? true : false
   end
 
   def self.position_empty?(move)
-    @positions[move - 1].is_a?(Integer)
+    @@positions[move - 1].is_a?(Integer)
   end
 
   def self.draw
-    'Game over & it is a draw!!' if @positions.none?(Integer)
+    'Game over & it is a draw!!' if @@positions.none?(Integer)
   end
 
   def self.make_move(move, mark)
@@ -33,8 +41,13 @@ class Board
 
     return "Position #{move} has been taken, please select another. >> " unless position_empty?(move)
 
-    @positions[move - 1] = mark
+    @@positions[move - 1] = mark
+  end
+
+  def self.reset_board
+    @@positions.size.times { |el| @@positions[el] = el + 1 }
   end
 end
 
-Board.make_move(2, "X")
+puts Board.make_move(1, 'x')
+puts Board.make_move(1, 'x')
